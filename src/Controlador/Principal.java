76c259da4bfdbaf52234a643d1c973cd4c7ff781
jonathan <jonathan@10.211.55.5>
2019-10-28 00:25:14 -0500

    Version inicial.

--- src/Controlador/Principal.java
+++ src/Controlador/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		//JONATHAN ARIZA PEÑA
 		//DAVID LEONARDO BARRERA LOPEZ
-		// NOMBRE FALTANTE 
+		//LUIS ESTEBAN NIÑO
 		Controlador c = new Controlador();
 	}
 
